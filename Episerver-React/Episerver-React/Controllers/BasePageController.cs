8a76ed1f0dac704372fa51f92d586085f6072875
andreipv <30652343+andreipv@users.noreply.github.com>
2018-09-12 17:01:34 +0300

    Site Settings block

--- Episerver-React/Episerver-React/Controllers/BasePageController.cs
+++ Episerver-React/Episerver-React/Controllers/BasePageController.cs
@@ -1,14 +1,28 @@
-﻿using EPiServer;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Web;
+using System.Web.Caching;
+using EPiServer;
+using EPiServer.Core;
+using EPiServer.Logging;
+using EPiServer.ServiceLocation;
 using EPiServer.Web.Mvc;
+using Episerver_React.Models.Blocks;
 using Episerver_React.Models.Interfaces;
 using Episerver_React.Models.Pages;
 using Episerver_React.Models.ViewModels;
-using System;
 
 namespace Episerver_React.Controllers
 {
     public abstract class BasePageController<T> : PageController<T> where T : BasePageData
     {
+        internal Injected<IContentRepository> _contentRepo;
+
+
+
         /// <summary>
         /// Creates the simple page view mode
         /// </summary>
@@ -57,7 +71,56 @@ namespace Episerver_React.Controllers
             {
                 return;
             }
+
+            var homePage = GetHomePage(model.CurrentPage);
+            var siteSettings = GetSiteSettings(homePage);
+
+            if (siteSettings == null)
+            {
+                return;
+            }
+            
+            
+            
+
         }
+
+
+        private HomePage GetHomePage(BasePageData page)
+        {
+            if (page == null || _contentRepo.Service == null)
+            {
+                return null;
+            }
+
+            //checks if this is the home page
+            var homePage = page as HomePage;
+            if (homePage != null)
+            {
+                return homePage;
+            }
+
+            _contentRepo.Service.TryGet(ContentReference.StartPage, out homePage);
+            return homePage;
+        }
+
+
+        private SiteSettingsBlock GetSiteSettings(HomePage page)
+        {
+            if (page == null || _contentRepo.Service == null)
+            {
+                return null;
+            }
+            if (ContentReference.IsNullOrEmpty(page.SiteSettings) == null)
+            {
+                return null;
+            }
+
+            SiteSettingsBlock settings = null;
+            _contentRepo.Service.TryGet(page.SiteSettings, out settings);
+            return settings;
+        }
+
         #endregion
     }
 }
---
+++ Episerver-React/Episerver-React/Models/Blocks/SiteSettingsBlock.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using Episerver_React.Business.Settings;
+using Episerver_React.Models.Pages;
+
+namespace Episerver_React.Models.Blocks
+{
+    [ContentType(DisplayName = "Site Settings Block", GUID = "046c16a7-c9f4-497d-998d-c19d134ca030", Description = "")]
+    public class SiteSettingsBlock : BlockData
+    {
+        [CultureSpecific]
+        [Display(
+                   Name = "Footer Disclaimer",
+                   Description = "Footer Disclaimer",
+                   GroupName = SystemTabNames.Settings,
+                   Order = 10)]
+        public virtual string FooterDisclaimer { get; set; }
+
+        [Display(
+           Name = "Header",
+           Order = 20,
+           GroupName = ContentEditorTabs.Header)]
+        public virtual HtmlInjectedBlock HeaderBlock { get; set; }
+
+        [Display(
+            Name = "Footer",
+            Order = 30,
+            GroupName = ContentEditorTabs.Footer)]
+        public virtual HtmlInjectedBlock FooterBlock { get; set; }
+    }
+}
